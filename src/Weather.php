71f29b98a59bf4ac8d43bf7828430aa86a51c547
YuXin Zhai <zhaiyuxin103@hotmail.com>
2025-08-15 20:40:31 +0800

    test: 增加单元测试

--- src/Weather.php
+++ src/Weather.php
@@ -36,14 +36,17 @@ class Weather
     {
         $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
 
+        // 对 `$format` 进行检查，如果不是 `xml` 或 `json` 则抛出异常
         if (!in_array(strtolower($format), ['xml', 'json'])) {
             throw new InvalidArgumentException('Invalid response format: ' . $format);
         }
 
+        // 对 `$type` 进行检查，如果不是 `base` 或 `all` 则抛出异常
         if (!in_array(strtolower($type), ['base', 'all'])) {
             throw new InvalidArgumentException('Invalid type value(base/all): ' . $type);
         }
 
+        // 构建查询参数
         $query = array_filter([
             'key' => $this->key,
             'city' => $city,
@@ -52,12 +55,15 @@ class Weather
         ]);
 
         try {
+            // 调用 getHttpClient 发送 HTTP 请求并返回结果
             $response = $this->getHttpClient()->get($url, [
                 'query' => $query,
             ])->getBody()->getContents();
 
+            // 根据 `$format` 返回 JSON 或 XML 数据
             return 'json' === $format ? json_decode($response, true) : $response;
         } catch (Exception $e) {
+            // 捕获异常并抛出 HttpException
             throw new HttpException($e->getMessage(), $e->getCode(), $e);
         }
     }
---
+++ tests/Feature/WeatherTest.php
@@ -0,0 +1,110 @@
+<?php
+
+use GuzzleHttp\Client;
+use Yuxin\Weather\Weather;
+use Mockery\Matcher\AnyArgs;
+use GuzzleHttp\Psr7\Response;
+use Yuxin\Weather\Exceptions\InvalidArgumentException;
+
+describe('Weather', function () {
+    test('get http client', function () {
+        $weather = new Weather('mock-key');
+        expect($weather->getHttpClient())->toBeInstanceOf(Client::class);
+    });
+
+    test('set guzzle options', function () {
+        $weather = new Weather('mock-key');
+
+        // 设置参数前，timeout 为 null
+        expect($weather->getHttpClient()->getConfig('timeout'))->toBeNull();
+
+        // 设置参数
+        $weather->setGuzzleOptions(['timeout' => 50000]);
+
+        // 设置参数后，timeout 为 50
+        expect($weather->getHttpClient()->getConfig('timeout'))->toBe(50000);
+    });
+});
+
+describe('Get Weather', function () {
+    describe('invalid argument', function () {
+        // 测试 $type 参数
+        test('type', function () {
+            expect(function () {
+                $weather = new Weather('mock-key');
+                $weather->getWeather('北京', 'foo');
+            })->toThrow(InvalidArgumentException::class, 'Invalid type value(base/all): foo');
+        });
+
+        // 测试 $format 参数
+        test('format', function () {
+            expect(function () {
+                $weather = new Weather('mock-key');
+                $weather->getWeather('北京', 'base', 'array');
+            })->toThrow(InvalidArgumentException::class, 'Invalid response format: array');
+        });
+    });
+
+    describe('return', function () {
+        test('json', function () {
+            // 创建模拟接口响应值
+            $response = new Response(200, [], '{"success": true}');
+
+            // 创建模拟的 Guzzle HTTP 客户端
+            $client = Mockery::mock(Client::class);
+
+            // 指定将会产生的行为（在后续的测试中将会按下面的参数来调用）
+            $client->allows()->get('https://restapi.amap.com/v3/weather/weatherInfo', [
+                'query' => [
+                    'key' => 'mock-key',
+                    'city' => '北京',
+                    'output' => 'json',
+                    'extensions' => 'base',
+                ],
+            ])->andReturn($response);
+
+            // 将 `getHttpClient` 方法替换为使用模拟客户端
+            $weather = Mockery::mock(Weather::class, ['mock-key'])->makePartial();
+            // $client 为上面创建的模拟实例
+            $weather->allows()->getHttpClient()->andReturn($client);
+
+            // 然后调用 `getWeather` 方法，并断言返回值为模拟的返回值
+            expect($weather->getWeather('北京'))->toBe(['success' => true]);
+        });
+
+        test('xml', function () {
+            $response = new Response(200, [], '<hello>world</hello>');
+
+            $client = Mockery::mock(Client::class);
+
+            $client->allows()->get('https://restapi.amap.com/v3/weather/weatherInfo', [
+                'query' => [
+                    'key' => 'mock-key',
+                    'city' => '北京',
+                    'output' => 'xml',
+                    'extensions' => 'all',
+                ],
+            ])->andReturn($response);
+
+            $weather = Mockery::mock(Weather::class, ['mock-key'])->makePartial();
+            $weather->allows()->getHttpClient()->andReturn($client);
+
+            expect($weather->getWeather('北京', 'all', 'xml'))->toBe('<hello>world</hello>');
+        });
+    });
+
+    describe('exception', function () {
+        test('guzzle runtime', function () {
+            // 接着需要断言调用时会产生异常
+            expect(function () {
+                $client = Mockery::mock(Client::class);
+                $client->allows()->get(new AnyArgs())->andThrow(new Exception('request timeout'));
+
+                $weather = Mockery::mock(Weather::class, ['mock-key'])->makePartial();
+                $weather->allows()->getHttpClient()->andReturn($client);
+
+                $weather->getWeather('北京');
+            })->toThrow(Exception::class, 'request timeout');
+        });
+    });
+});
