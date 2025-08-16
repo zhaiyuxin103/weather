31e8c9aae7ebc9d91b232eade421a4b63d5749fc
YuXin Zhai <zhaiyuxin103@hotmail.com>
2025-08-16 19:48:38 +0800

    chore(laravel): service provider

---
+++ src/ServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Yuxin\Weather;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use Illuminate\Contracts\Support\DeferrableProvider;
+
+class ServiceProvider extends BaseServiceProvider implements DeferrableProvider
+{
+    /**
+     * 标记着提供器是延迟加载的
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot()
+    {
+        return [Weather::class, 'weather'];
+    }
+
+    public function register()
+    {
+        $this->app->singleton(Weather::class, function () {
+            return new Weather(config('services.weather.key'));
+        });
+
+        $this->app->alias(Weather::class, 'weather');
+    }
+
+    /**
+     * Get the services provided by the provider.
+     * 
+     * @return array<int, string>
+     */
+    public function provides(): array
+    {
+        return [Weather::class];
+    }
+}
