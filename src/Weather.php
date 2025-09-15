a4812f750278c02d1748184e04b36554c75e435a
YuXin Zhai <zhaiyuxin103@hotmail.com>
2025-09-15 17:29:08 +0800

    perf(weather): 在 Weather 类中添加常量和单例模式的 HTTP 客户端

--- src/Weather.php
+++ src/Weather.php
@@ -12,9 +12,26 @@ use Yuxin\Weather\Exceptions\InvalidArgumentException;
 
 class Weather
 {
+    public const API_URL = 'https://restapi.amap.com/v3/weather/weatherInfo';
+
+    public const TYPE_LIVE = 'live';
+
+    public const TYPE_FORECAST = 'forecast';
+
+    public const FORMAT_JSON = 'json';
+
+    public const FORMAT_XML = 'xml';
+
+    private const TYPE_MAPPING = [
+        self::TYPE_LIVE     => 'base',
+        self::TYPE_FORECAST => 'all',
+    ];
+
     protected string $key;
 
     protected array $guzzleOptions = [];
+
+    protected ?Client $httpClient = null;
 
     public function __construct(string $key)
     {
@@ -23,12 +40,18 @@ class Weather
 
     public function getHttpClient(): Client
     {
-        return new Client($this->guzzleOptions);
+        if ($this->httpClient === null) {
+            $this->httpClient = new Client($this->guzzleOptions);
+        }
+
+        return $this->httpClient;
     }
 
     public function setGuzzleOptions(array $options): void
     {
         $this->guzzleOptions = $options;
+        // Reset client instance when options change
+        $this->httpClient = null;
     }
 
     /**
@@ -36,21 +59,19 @@ class Weather
      * @throws GuzzleException
      * @throws HttpException
      */
-    public function getWeather($city, string $type = 'live', string $format = 'json')
-    {
-        $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
-
-        $types = [
-            'live'     => 'base',
-            'forecast' => 'all',
-        ];
+    public function getWeather(
+        string $city,
+        string $type = self::TYPE_LIVE,
+        string $format = self::FORMAT_JSON
+    ) {
+        $types = self::TYPE_MAPPING;
 
         // 对 `$format` 进行检查，如果不是 `xml` 或 `json` 则抛出异常
-        if (! in_array(strtolower($format), ['xml', 'json'])) {
+        if (! in_array(strtolower($format), [self::FORMAT_XML, self::FORMAT_JSON])) {
             throw new InvalidArgumentException('Invalid response format: ' . $format);
         }
 
-        // 对 `$type` 进行检查，如果不是 `base` 或 `all` 则抛出异常
+        // 对 `$type` 进行检查，如果不是 `live` 或 `forecast` 则抛出异常
         if (! array_key_exists(strtolower($type), $types)) {
             throw new InvalidArgumentException('Invalid type value(live/forecast): ' . $type);
         }
@@ -65,25 +86,25 @@ class Weather
 
         try {
             // 调用 getHttpClient 发送 HTTP 请求并返回结果
-            $response = $this->getHttpClient()->get($url, [
+            $response = $this->getHttpClient()->get(self::API_URL, [
                 'query' => $query,
             ])->getBody()->getContents();
 
             // 根据 `$format` 返回 JSON 或 XML 数据
-            return $format === 'json' ? json_decode($response, true) : $response;
+            return $format === self::FORMAT_JSON ? json_decode($response, true) : $response;
         } catch (Exception $e) {
             // 捕获异常并抛出 HttpException
             throw new HttpException($e->getMessage(), $e->getCode(), $e);
         }
     }
 
-    public function getLiveWeather($city, string $format = 'json')
+    public function getLiveWeather(string $city, string $format = self::FORMAT_JSON)
     {
-        return $this->getWeather($city, 'live', $format);
+        return $this->getWeather($city, self::TYPE_LIVE, $format);
     }
 
-    public function getForecastsWeather($city, string $format = 'json')
+    public function getForecastsWeather(string $city, string $format = self::FORMAT_JSON)
     {
-        return $this->getWeather($city, 'forecast', $format);
+        return $this->getWeather($city, self::TYPE_FORECAST, $format);
     }
 }
